1e5bcc3c690e02b8fecda668616c1ef982c9bee8
smasher44 <georgelibatique819@gmail.com>
2022-01-26 00:33:34 +0800

    added particle js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import Particles from "react-tsparticles";
+
+const particlesOptions = {
+    fpsLimit: 120,
+    particles: {
+      color: {
+        value: "#0ccac4",
+      },
+      links: {
+        color: "#0ccac4",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 1,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          value_area: 1800,
+        },
+        value: 90,
+      },
+      opacity: {
+        value: 0.9,
+      },
+      size: {
+        random: true,
+        value: 3,
+      },
+    },
+}
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
     if(isSignedIn){
         return(
             <nav style = {{display: 'flex', justifyContent: 'flex-end'}}>
+            <Particles id="tsparticles" options={particlesOptions} /> 
             <p onClick={() => onRouteChange('signout')} 
                className = 'f3 link dim black underline pa3 pointer'>Sign Out</p>
             </nav>
@@ -11,6 +54,7 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
     } else {
         return(
             <nav style = {{display: 'flex', justifyContent: 'flex-end'}}>
+            <Particles id="tsparticles" options={particlesOptions} /> 
             <p onClick={() => onRouteChange('signin')} 
                className = 'f3 link dim black underline pa3 pointer'>Sign In</p>
             <p onClick={() => onRouteChange('register')} 
